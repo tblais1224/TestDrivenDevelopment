2acfacf504969fab485006fc24db75a86819e27e
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-16 14:01:04 -0400

    Add project files.

---
+++ TestDrivenDevelopment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopment
+{
+    public class NaiveCanonicalizer
+    {
+        public static string GetCanonicalForm(string searchTerm)
+        {
+            return searchTerm
+                .Split(new[] { ' ' })
+                .Select(x => x.ToUpper())
+                .OrderBy(x => x)
+                .Aggregate((x, y) => x + " " + y);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
