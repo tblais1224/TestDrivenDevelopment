255c3abaf4171a8d4875e5053a8f049c9f05d92f
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-17 13:21:49 -0400

    fizz buzzdone

---
+++ Tests/FizzBuzzTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [TestCase("Fizz", 3)]
+        [TestCase("Fizz", 6)]
+        [TestCase("Buzz", 5)]
+        [TestCase("Buzz", 10)]
+        [TestCase("FizzBuzz", 15)]
+        [TestCase("FizzBuzz", 30)]
+        [TestCase("", 7)]
+        public void TestFizzBuzz(string expected, int number)
+        {
+            Assert.AreEqual(expected, FizzBuzz(number));
+        }
+
+        private string FizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return "";
+        }
+    }
+}
