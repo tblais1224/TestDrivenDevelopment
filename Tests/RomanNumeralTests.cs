1ba534320d0dd84ddc739f69352db7807e8e48fa
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-17 13:57:57 -0400

    vid 22 roman numerals is done

---
+++ Tests/RomanNumeralTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class RomanNumeralTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(5, "V")]
+        [TestCase(10, "X")]
+        [TestCase(2, "II")]
+        [TestCase(4, "IV")]
+        public void Parse(int expected, string roman)
+        {
+            Assert.AreEqual(expected, Roman.Parse(roman));
+        }
+    }
+
+    public class Roman
+    {
+        private static Dictionary<char, int> _romanNumerals = new Dictionary<char, int>()
+        {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+
+        public static int Parse(string roman)
+        {
+            int result = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && IsSubtractive(roman[i], roman[i + 1]))
+                {
+                    result -= _romanNumerals[roman[i]];
+                }
+                else
+                {
+                    result += _romanNumerals[roman[i]];
+                }
+            }
+            return result;
+        }
+
+        private static bool IsSubtractive(char c1, char c2)
+        {
+            return _romanNumerals[c1] < _romanNumerals[c2];
+        }
+    }
+}
