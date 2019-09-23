e08ee0e17efd717c8ccf6e5ec3698a43aa8c8349
tblais1224 <thomasblais.student@careerdevs.com>
2019-09-23 17:53:11 -0400

    working on vid 35

---
+++ Tests/Business/Sut.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+
+namespace Tests.Business
+{
+    public class Customer
+    {
+        private readonly Logger _logger = new Logger();
+
+        public decimal CalculateWage(int id)
+        {
+            DbGateway gateway = new DbGateway();
+            WorkingStatistics ws = gateway.GetWorkingStatistics(id);
+
+            decimal wage;
+            if (ws.PaytHourly)
+            {
+                wage = ws.WorkingHours * ws.HourSalary;
+            }
+            else
+            {
+                wage = ws.MonthlySalary;
+            }
+
+            _logger.Info($"Customer ID={id}, Wage:{wage}");
+            return wage;
+        }
+    }
+}
